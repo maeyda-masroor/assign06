27292e36d2a568c0f336a5856ed4f7706c0ff383
freelancing with programming <iosattbsoft2021@gmail.com>
2025-05-01 19:47:36 +0500

    ccxx

---
+++ 1.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    
+    def display(self):
+        print(self.name)
+        print(self.makrs)
+    
+student = Student()
+student.setDetail("Ali","90")
+
+
